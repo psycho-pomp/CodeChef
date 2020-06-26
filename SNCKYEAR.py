00b98f2bfc8a1a772b84d768c422b04312261987
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2020-06-26 23:57:14 +0530

    Chef and SnackDown

---
+++ SNCKYEAR.py
@@ -0,0 +1,8 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    n=int(input())
+    if n in [2010,2015,2016,2017,2019]:
+        print("HOSTED")
+    else:
+        print("NOT HOSTED")
