ce68a246d4c625aff26df69b5d0b64f9fe28eb79
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2020-07-08 02:21:50 +0530

    Chef and An Ideal Problem

---
+++ CHFIDEAL.py
@@ -0,0 +1,7 @@
+# cook your dish here
+# copied from editorial as this was my first interacive problem
+print(1,flush=True)
+if(int(input())==2):
+    print(3,flush=True)
+else:
+    print(2,flush=True)
