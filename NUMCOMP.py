bc49b467bb39bd9c27896b1f847a17131d73b0f5
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2020-08-26 14:50:33 +0530

    Archi and Comparsion

---
+++ NUMCOMP.py
@@ -0,0 +1,21 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    a,b,n=map(int,input().split())
+    if n%2==0:
+        a=abs(a)
+        b=abs(b)
+        if a>b:
+            print(1)
+        elif b>a:
+            print(2)
+        else:
+            print(0)
+    else:
+        if a>b:
+            print(1)
+        elif b>a:
+            print(2)
+        else:
+            print(0)
+        
